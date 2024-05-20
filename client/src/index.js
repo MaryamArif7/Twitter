3a17c9b666f4fb0bea5f651b133f57eedbaeef2d
MaryamArif7 <www.maryamarif28242@gmail.com>
2024-05-20 10:18:38 +0500

    fourth

--- client/src/index.js
+++ client/src/index.js
@@ -43,7 +43,9 @@ auth reducer is the root reducer
 the state will be persist based on provided reducer and rehydrate on the base of the persistconfi
 */}
 const persistedReducer=persistReducer(persistConfig,authReducer);
-{/*3->confiure store a redux tolkit function which takes persisted reducer and other */}
+{/*3->confiure store a redux tolkit function which takes persisted reducer(basiaclly store takes reducers functions but in this case we are using oersist redux so 
+  we are takin the reducer function which is persisted 
+ ) and other */}
 const store=configureStore({
   //root reducer 
   reducer:persistedReducer,
@@ -64,9 +66,11 @@ getDefaultMiddleware({
 //persistor:is an object created by redux persist ,which handles the peristence of the store 
 //basically what it does is that ,it saves the redux state to storge and rehydate it when the app starts aggain
 //persist gate delay the renrendering to wait for the persisted state to eb restore 
+//providin store to the whole app 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+     
     <Provider store={store}>
 
       <PersistGate loading={null}  persistor={persistStore(store)}>
@@ -76,3 +80,6 @@ root.render(
 
   </React.StrictMode>
 );
+/*
+
+ */
