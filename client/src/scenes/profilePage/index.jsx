785f148a7fee585b01f91ff14d82355582dd2b5b
MaryamArif7 <www.maryamarif28@gmail.com>
2024-05-21 12:46:20 +0500

    sixth

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -7,9 +7,24 @@ import FriendListWidget from "scenes/widgets/FriendListWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
 import UserWidget from "scenes/widgets/UserWidget";
-
+/**
+ function orders:
+ ->ProiflePage
+ ->useEffect
+ ->getUser
+ ->serverRes
+ ->getUser process the response and rerender the compenent*/
 const ProfilePage = () => {
   const [user, setUser] = useState(null);
+  /*Use Param:
+ -> a route defined in react router Dom,which includes dynmic segment means id etc which can be 
+different for every single person,then the value of dynamc sement can be accessed 
+using useParams
+  ->When the application's current URL matches a route with a dynamic segment, React Router DOM
+   parses the URL and extracts the dynamic segment's value.
+  >we user the param hook  to acess the paramentrs extracted
+  from URL,so extracted key into userId
+  */
   const { userId } = useParams();
   const token = useSelector((state) => state.token);
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -17,15 +32,26 @@ const ProfilePage = () => {
   const getUser = async () => {
     const response = await fetch(`http://localhost:3001/users/${userId}`, {
       method: "GET",
+      //checki if the token is still valid
       headers: { Authorization: `Bearer ${token}` },
     });
     const data = await response.json();
     setUser(data);
   };
+/*
+1.component mount:
+ ->when the profilePage mounts >renders for the first time ,the useEffect s triggered
+->the getUser function call is inside of the useEffect hook,
+then this will call the http request too server to fetcht he data
+->after http req is completed ,when the res is rec,then the getUser will
+update the funnction state to setUser
+->useEffect dependc array is empty meaning it will run only when the 
+copeonent is mounted when the page is rendered first.
 
+*/
   useEffect(() => {
     getUser();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []); 
 
   if (!user) return null;
 
