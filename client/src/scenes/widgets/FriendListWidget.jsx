2982b8fb1216ec95b6df00639a8e78f64c280029
MaryamArif7 <www.maryamarif28242@gmail.com>
2024-05-23 17:44:48 +0500

    eight

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -4,7 +4,12 @@ import WidgetWrapper from "components/WidgetWrapper";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setFriends } from "state";
-
+//takin the prop from the ->scenes->homepage->index .js where parent is defined
+//child of another parent
+//this child is getting the props of userId fromt the homepage parent 
+//then extracting the  the porperties of freinds usin the id 
+//then this is also parent compennt for the "Friends" and 
+//passing the props to that child component.
 const FriendListWidget = ({ userId }) => {
   const dispatch = useDispatch();
   const { palette } = useTheme();
@@ -19,6 +24,10 @@ const FriendListWidget = ({ userId }) => {
         headers: { Authorization: `Bearer ${token}` },
       }
     );
+    //this dispatched functio is called and it dispcthes an ction
+    //with the type setFriends and the pyload conatinig the friends data 
+    //fetched from the server 
+    //then this action is passed t the reucer and reducer will update the state accordinnly.
     const data = await response.json();
     dispatch(setFriends({ friends: data }));
   };
