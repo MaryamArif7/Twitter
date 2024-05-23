2851de1dad8f125d0bd8f61506d0ae40887342b8
MaryamArif7 <www.maryamarif28242@gmail.com>
2024-05-23 16:46:07 +0500

    seventh

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -14,8 +14,34 @@ export const getUser = async (req, res) => {
 export const getUserFriends = async (req, res) => {
   try {
     const { id } = req.params;
+    //User->Modal
+    //->id is the id extracted from the params
     const user = await User.findById(id);
+    //promise.all takes an arry of promises which are returned by the id of the user 
+    //and returns a single promise whehn all oll of the input promises have been resolved
+    //then await waits for all the all the promises and ssins the value to the frieds 
+
     const friends = await Promise.all(
+      /*the user =>obj,is the which we got from the db of User after finding it by id 
+     -> user has proptery of freinds->a  array->value in the User
+      ->map is a fun which creates a new array by applying a function to
+      ->ech eelemnt of the orignal array 
+     -> then user.friends ->are friends Ids
+     -> then map apllied the function to them
+     -> (id)=>User.findById(id) this is an arraow function that takes one agument->id
+     ->and returns the result of User.findByid(id)
+     ->this function is applied to each Id in the user.friends array
+     basically:
+     code takes the friends array from the user object, 
+     which contains friend IDs,
+      and uses map to transform each ID into a user object 
+     by fetching it from the database using User.findById(id).
+     ->transforming ecah ID into a user object:
+     ->databse call for each elemnt in the frinds array 
+     call bcak function is the databse query here 
+     amd we are  not  disrectly converting them into user objs,but
+     returning an array of promise 
+      */
       user.friends.map((id) => User.findById(id))
     );
 
